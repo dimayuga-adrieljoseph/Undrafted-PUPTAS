14cf2aa161e2020bb3a50b20f9670fd30e8718c6
Dion Kylo C. Manicio <145413588+manicio-dion@users.noreply.github.com>
2025-12-27 20:39:46 +0800

    Make seeders idempotent (#8)

--- puptas/database/seeders/ProgramSeeder.php
+++ puptas/database/seeders/ProgramSeeder.php
@@ -13,14 +13,17 @@ class ProgramSeeder extends Seeder
      */
     public function run(): void
     {
-        
+
         $programs = [
             ['code' => 'BSIT', 'name' => 'Bachelor of Science in Information Technology', 'strand' => 'STEM', 'math' => 85, 'science' => 85, 'english' => 85, 'gwa' => 90, 'pupcet' => 120, 'slots' => 50],
             ['code' => 'BSBA-HRM', 'name' => 'BSBA - Human Resource Management', 'strand' => 'ABM', 'math' => 80, 'science' => 80, 'english' => 80, 'gwa' => 85, 'pupcet' => 120, 'slots' => 30],
         ];
 
         foreach ($programs as $program) {
-            Program::create($program);
+            Program::updateOrCreate(
+                ['code' => $program['code']],
+                $program
+            );
         }
     }
 }
