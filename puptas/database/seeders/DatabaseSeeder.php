71f6deb1b9e51ddbd50a17478093b5144ab0b948
Dion Manicio <dionkylo123@gmail.com>
2025-12-27 20:28:50 +0800

    Make seeders idempotent

--- puptas/database/seeders/DatabaseSeeder.php
+++ puptas/database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Run ProgramSeeder first if users depend on programs
+        // Seed reference data first, then users (custom users table has no `name` column)
         $this->call([
             ProgramSeeder::class,
             UserSeeder::class,
