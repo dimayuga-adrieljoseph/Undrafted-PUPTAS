f962fa040ae10f2674492d7e3b642143c54cc2d5
dimayuga-adrieljoseph <dimayuga.adrieljoseph03@gmail.com>
2026-03-03 20:43:05 +0800

    docker update

--- puptas/docker-entrypoint.sh
+++ puptas/docker-entrypoint.sh
@@ -9,7 +9,7 @@ rm -f /etc/apache2/mods-enabled/mpm_event.* /etc/apache2/mods-enabled/mpm_worker
 a2enmod mpm_prefork
 
 # Listen on Railway's $PORT
-echo "Listen ${PORT:-80}" >> /etc/apache2/ports.conf
+echo "Listen ${PORT:-8080}" >> /etc/apache2/ports.conf
 
 # Test config and start
 apache2ctl -t
