44b5a90446a742b0c7c36b10e835966348b71f69
dimayuga-adrieljoseph <dimayuga.adrieljoseph03@gmail.com>
2026-03-04 03:25:03 +0800

    docker update

--- puptas/vite.config.js
+++ puptas/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
+            buildDirectory: 'build',
         }),
         vue({
             template: {
@@ -20,9 +22,12 @@ export default defineConfig({
     base: '/',
     build: {
         manifest: true,
+        outDir: 'public/build',
+        emptyOutDir: true,
     },
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'resources/js'),
+        },
     },
 });
