f522dc037e51684e3ee05fe7df1856a766afd877
dimayuga-adrieljoseph <dimayuga.adrieljoseph03@gmail.com>
2026-03-12 02:25:29 +0800

    update

--- puptas/routes/web.php
+++ puptas/routes/web.php
@@ -56,7 +56,7 @@ Route::get('/applications/user/{user}', function ($user) {
     return Inertia::render('Applications/Index', [
         'selectedUserId' => (int) $user
     ]);
-})->middleware(['auth', 'role:2,3,4'])->whereNumber('user')->name('applications.show');
+})->middleware(['auth', 'role:2,3,4,7'])->whereNumber('user')->name('applications.show');
 
 Route::post('/check-email', function (\Illuminate\Http\Request $request) {
     $request->validate(['email' => 'required|email']);
@@ -346,8 +346,8 @@ Route::middleware(['auth', 'role:6'])->group(function () {
     Route::post('/record-dashboard/return-files/{user}', [RecordStaffDashboardController::class, 'returnApplication'])->name('record-return.files');
 });
 
-// Shared endpoint for viewing user list - accessible by admin, evaluator, and interviewer
-Route::middleware(['auth', 'role:2,3,4'])->group(function () {
+// Shared endpoint for viewing user list - accessible by admin, evaluator, interviewer, and superadmin
+Route::middleware(['auth', 'role:2,3,4,7'])->group(function () {
     Route::get('/dashboard/users', [DashboardController::class, 'getUsers']);
 });
 
