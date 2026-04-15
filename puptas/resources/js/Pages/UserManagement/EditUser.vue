8b5d9318c12cbc800d33bc5d44b55437f74d76e9
Dion Manicio <dionkylo123@gmail.com>
2026-04-15 11:44:08 +0800

    removed the "Security (Optional)" section

--- puptas/resources/js/Pages/UserManagement/EditUser.vue
+++ puptas/resources/js/Pages/UserManagement/EditUser.vue
@@ -19,17 +19,12 @@ const form = useForm({
     extension_name: props.user.extension_name || '',
     email: props.user.email || '',
     contactnumber: props.user.contactnumber || '',
-    password: '',
-    password_confirmation: '',
     role_id: props.user.role_id || '',
     // Use an array of program CODEs
     program: props.user.programs && props.user.programs.length > 0 ? props.user.programs.map(p => p.code) : [],
     // For applicants: use single selection
     applicant_program: props.user.programs && props.user.programs.length > 0 ? props.user.programs[0].code : ''
 });
-
-const showPassword = ref(false);
-const showConfirmPassword = ref(false);
 
 const showProgramAssignment = computed(() => {
     return ['3', '4'].includes(form.role_id.toString()); // Evaluator (3) and Interviewer (4)
@@ -302,62 +297,6 @@ const submitForm = () => {
                                             <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/>
                                         </svg>
                                         {{ form.errors.contactnumber }}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Security Section -->
-                        <div class="form-section">
-                            <h3 class="section-title">Security (Optional)</h3>
-                            <p class="section-description">Leave password fields blank to keep current password</p>
-                            
-                            <div class="form-grid flex flex-col md:flex-row gap-4">
-                                <div class="form-group">
-                                    <label for="password" class="form-label">New Password</label>
-                                    <div class="password-input">
-                                        <input 
-                                            id="password" 
-                                            v-model="form.password" 
-                                            :class="['form-input w-full', { 'error': form.errors.password }]"
-                                            :type="showPassword ? 'text' : 'password'"
-                                            placeholder="Enter new password (optional)"
-                                        />
-                                        <button 
-                                            type="button" 
-                                            class="password-toggle"
-                                            @click="showPassword = !showPassword"
-                                        >
-                                            <svg v-if="showPassword" viewBox="0 0 24 24">
-                                                <path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z"/>
-                                            </svg>
-                                            <svg v-else viewBox="0 0 24 24">
-                                                <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
-                                            </svg>
-                                        </button>
-                                    </div>
-                                    <div v-if="form.errors.password" class="form-error">
-                                        <svg class="error-icon" viewBox="0 0 24 24">
-                                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/>
-                                        </svg>
-                                        {{ form.errors.password }}
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="password_confirmation" class="form-label">Confirm Password</label>
-                                    <input 
-                                        id="password_confirmation" 
-                                        v-model="form.password_confirmation" 
-                                        :class="['form-input w-full', { 'error': form.errors.password_confirmation }]"
-                                        :type="showConfirmPassword ? 'text' : 'password'"
-                                        placeholder="Confirm new password"
-                                    />
-                                    <div v-if="form.errors.password_confirmation" class="form-error">
-                                        <svg class="error-icon" viewBox="0 0 24 24">
-                                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/>
-                                        </svg>
-                                        {{ form.errors.password_confirmation }}
                                     </div>
                                 </div>
                             </div>
