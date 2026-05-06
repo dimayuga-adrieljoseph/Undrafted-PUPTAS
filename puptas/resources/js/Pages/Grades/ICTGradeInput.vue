c903d9c9aa79944059dcb9e33015936a9c83bf1b
dazo-rollan <olandazo37@gmail.com>
2026-05-06 22:37:54 +0800

    SCRUM-86: grades review confirmation modal

--- puptas/resources/js/Pages/Grades/ICTGradeInput.vue
+++ puptas/resources/js/Pages/Grades/ICTGradeInput.vue
@@ -31,7 +31,7 @@
                 </div>
             </div>
 
-            <form @submit.prevent="submitForm">
+            <form @submit.prevent="openReviewModal">
                 <!-- AI Autofill Banner -->
                 <div v-if="extractionResult && !bannerDismissed" class="mb-6 p-4 bg-blue-50 dark:bg-blue-900/20 border border-blue-200 dark:border-blue-800 rounded-lg flex items-center justify-between gap-4">
                     <div class="flex items-center gap-2">
@@ -652,6 +652,14 @@
                     </div>
                 </transition>
             </form>
+
+            <GradesReviewModal
+                :show="showReviewModal"
+                :loading="loading"
+                :form-data="form"
+                @close="closeReviewModal"
+                @confirm="confirmSaveGrades"
+            />
         </div>
     </ApplicantLayout>
 </template>
@@ -660,6 +668,7 @@
 import { ref, reactive, computed, onMounted } from "vue";
 import { usePage, router } from "@inertiajs/vue3";
 import ApplicantLayout from "@/Layouts/ApplicantLayout.vue";
+import GradesReviewModal from "@/Components/GradesReviewModal.vue";
 
 const page = usePage();
 const props = defineProps({
@@ -951,6 +960,22 @@ onMounted(() => {
         applyAutofill(props.extractionResult);
     }
 });
+
+const openReviewModal = () => {
+    errors.value = {};
+    showReviewModal.value = true;
+};
+
+const closeReviewModal = () => {
+    if (!loading.value) {
+        showReviewModal.value = false;
+    }
+};
+
+const confirmSaveGrades = () => {
+    showReviewModal.value = false;
+    submitForm();
+};
 
 const submitForm = async () => {
     loading.value = true;
