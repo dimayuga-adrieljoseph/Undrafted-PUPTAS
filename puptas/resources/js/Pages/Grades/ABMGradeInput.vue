2cc48c219dc77ba4e2435ac9d2f8823628233361
Rollan Dazo <olandazo37@gmail.com>
2026-05-15 10:03:06 +0800

    Scrum 97 add program selection approval note (#170)
    * Update Check Status result page for qualifiers and non-passers
    
    * SCRUM-98: Redirect confirmed applicants to IDP registration with reminder modal
    
    * SCRUM-97: Add program selection approval notice to all grade input pages

--- puptas/resources/js/Pages/Grades/ABMGradeInput.vue
+++ puptas/resources/js/Pages/Grades/ABMGradeInput.vue
@@ -603,6 +603,20 @@
                     </div>
                     
                     <div class="p-6">
+                        <!-- Approval Notice -->
+                        <div class="flex items-start gap-3 rounded-xl border-2 border-[#9E122C] bg-red-50 p-4 mb-6 dark:border-red-700 dark:bg-red-900/20">
+                            <div class="flex-shrink-0 mt-0.5">
+                                <svg class="h-5 w-5 text-[#9E122C] dark:text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div>
+                                <p class="text-sm font-bold text-[#9E122C] dark:text-red-400">Important Notice</p>
+                                <p class="mt-1 text-sm text-red-800 dark:text-red-300">
+                                    This is subject to approval after the interview.
+                                </p>
+                            </div>
+                        </div>
                         <div v-if="qualifiedPrograms.length > 0">
                             <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                                 <div>
